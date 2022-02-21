fc8aad8405d92bce3dc02cf9978d543e48ccf4c5
Nareshbhai Mahto <82165555+NareshMahto@users.noreply.github.com>
2022-02-21 23:58:19 +0530

    Create Welcome to java.java

---
+++ Assignments/Hello World, Variables & Data Types/Welcome to java.java
@@ -0,0 +1,19 @@
+/*
+Welcome to the world of Java!
+
+In this challenge, you will learn how to print “Hello World!” in standard output (stdout).
+
+Complete the code by copying the two lines of code below and pasting them inside the body of your main method.
+
+System.out.println("Hello, World.");
+System.out.println("Hello, Java.");
+*/
+
+public class Main {
+    public static void main(String args[]) {
+        // your code here
+      System.out.println("Hello, World.");
+      System.out.println("Hello, Java.");
+      
+    }
+}
