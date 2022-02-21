ec78cda4aff53ea62f725d340f51c86e2c25aafa
Nareshbhai Mahto <82165555+NareshMahto@users.noreply.github.com>
2022-02-22 00:00:26 +0530

    Create Star Pattern.java

---
+++ Assignments/Hello World, Variables & Data Types/Star Pattern.java
@@ -0,0 +1,19 @@
+/*
+Star Pattern
+In this task, you need to print a right-angled triangle of height 3 (lines) and base 5 (stars).
+*
+***
+*****
+*/
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) {
+        //your code here
+      System.out.println("*");
+        System.out.println("***");
+          System.out.println("*****");
+    }
+}
