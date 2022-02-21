f1475e19131f61af4df9416d3bb45a48ccbe1e6d
Nareshbhai Mahto <82165555+NareshMahto@users.noreply.github.com>
2022-02-22 00:02:52 +0530

    Create Five Stars.java

---
+++ Assignments/Hello World, Variables & Data Types/Five Stars.java
@@ -0,0 +1,22 @@
+/*
+Five Stars
+In this task, you have to print 5 stars ("*") vertically and 5 horizontally.
+
+* * * * *
+*
+*
+*
+
+*/
+public class Main {
+    public static void main(String args[]) {
+        // your code here
+      System.out.println("* * * * *");
+      System.out.println("*");
+      System.out.println("*");
+      System.out.println("*");
+      System.out.println("*");
+      System.out.println("*");
+      
+    }
+}
