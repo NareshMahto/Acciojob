67bbf210b5782a10448b69e139ce99e44ce0b970
Nareshbhai Mahto <82165555+NareshMahto@users.noreply.github.com>
2022-03-16 07:21:04 +0530

    Create Max element in array.java

---
+++ Class Notes_code/Arrays/Max element in array.java
@@ -0,0 +1,24 @@
+/*
+find maximum elements in java
+-----------------------------
+*/
+
+import java.util.*;
+class NoofPair{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int [n];
+        for ( int i= 0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int maxx  = arr[0];
+        for (int i=0; i<n; i++){
+            if (arr[i] > maxx){
+                maxx = arr[i];
+            }
+        }
+
+    }
+}
