23bf399b37bf3ddc53d477bad0eba003db608fcd
Nareshbhai Mahto <82165555+NareshMahto@users.noreply.github.com>
2022-03-30 23:53:15 +0530

    Update Boolean Matrix Problem.java

--- Assignments/2D Arrays & Strings/Boolean Matrix Problem.java
+++ Assignments/2D Arrays & Strings/Boolean Matrix Problem.java
@@ -1,6 +1,7 @@
 /*
 Boolean Matrix Problem
-You are given a matrix Mat of m rows and n columns. The matrix is boolean so the elements of the matrix can only be either 0 or 1. Now, if any row of the matrix contains a 1, then you need to fill that whole row with 1. After doing the mentioned operation, you need to print the modified matrix.
+You are given a matrix Mat of m rows and n columns. The matrix is boolean so the elements of the matrix can only be either 0 or 1. Now, if any row of the
+matrix contains a 1, then you need to fill that whole row with 1. After doing the mentioned operation, you need to print the modified matrix.
 
 Input
 The first line of contains m and n denoting number of rows and number of columns. Then next m lines contain n elements denoting the elements of the matrix.
@@ -35,7 +36,8 @@ The given matrix is
 0 1 0 0
 0 0 0 0
 0 0 0 1
-Evidently, the first row contains a 1 so fill the whole row with 1. The third row also contains a 1 so that row will be filled too. Finally, the last row contains a 1 and therefore it needs to be filled with 1 too. The final matrix is
+Evidently, the first row contains a 1 so fill the whole row with 1. The third row also contains a 1 so that row will be filled too. Finally, 
+the last row contains a 1 and therefore it needs to be filled with 1 too. The final matrix is
 
 1 1 1 1
 0 0 0 0
